ee2f1da60ae182adb67223913a0a5d85df3b2f7c
David Theobald <mail@davidtheobald.de>
2025-10-13 14:50:11 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,6 +44,19 @@ export default function TabLayout() {
             ),
           }}
         />
+        <Tabs.Screen
+          name="artist"
+          options={{
+            href: null,
+          }}
+        />
+        <Tabs.Screen
+          name="player"
+          options={{
+            href: null,
+            tabBarStyle: { display: 'none' },
+          }}
+        />
       </Tabs>
   );
 }
