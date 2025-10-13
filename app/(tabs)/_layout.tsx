84dafde079577a79f1afeb3bd2504ea5fd0247c7
David Theobald <mail@davidtheobald.de>
2025-10-13 15:50:49 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
   },
   miniPlayerContainer: {
     position: 'absolute',
-    bottom: 55, // lift it above the tab bar
+    bottom: 45, // lift it above the tab bar
     left: 0,
     right: 0,
     zIndex: 10,
